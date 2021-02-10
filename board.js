de6553672e3843853e7a854a1a6945ced972c3ec
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-02-10 15:23:32 -0600

    Created the Grid

--- board.js
+++ board.js
@@ -1,13 +1,23 @@
 class Board {
-  constructor() {
+  constructor(numRows, numCols, numShips) {
     // TODO: Set up constructor that sets the numRos, numCols, and numShips.
     // TODO: Set this.grid equal to the return value of the instance method
     // populateGrid().
+    this.numRows = numRows;
+    this.numCols = numCols;
+    this.numShips = numShips;
   }
 
   populateGrid() {
     // TODO: Using the instance variables numRows, numCols, and numShips, return
     // a 2D array representing the state of the board.
+    let grid = [];
+    for (let i = 0; i < this.numRows; i++) {
+      grid.push([]);
+      for (let j = 0; j < this.numCols; j++) {
+        grid[i].push([j]);
+      }
+    }
   }
 
   display() {
--- humanPlayer.js
+++ humanPlayer.js
@@ -6,27 +6,28 @@ class HumanPlayer {
     // variable (this.rl). Remember to require the built-in node module,
     // 'readline'
     this.rl = readline.createInterface(process.stdin, process.stdout);
-
-
-
-
   }
 
   getMove(processMove) {
-    const rl = readline.createInterface(process.stdin, process.stdout);
-
-    rl.question("Give me two(2) numbers separated by a space (row, column).", (answer) => {
-      processMove(answer.split(' '))
-    });
+    this.rl.question(
+      "Choose a coordinate to hit. (e.g. 3,1) \n> ",
+      (answer) => {
+        const [row, col] = answer.split(",");
+        processMove([row, col]);
+      }
+    );
     // TODO: Ask the user for their move and process the answer using the rl
     // interface.Invoke the callback function (processMove), passing in
     // the given answer in the form of an array representing [row, col]
   }
 
-  processGameOver(isWon) {
+  processGameOver(isWon, turns) {
     if (isWon) {
-      rl.close();
+      console.log(`Nice job, you won in ${turns} turns!`);
+    } else {
+      console.log(`You lose!`);
     }
+    this.rl.close();
     // TODO: Display a different message depending on if the player won or lost
     // the game. Close the rl interface.
   }
