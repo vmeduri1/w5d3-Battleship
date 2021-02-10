508d5bc0fcce13109db343e7420b5c4c579b959e
jdaniel01 <professor4cob@outlook.com>
2021-02-10 12:59:44 -0800

    human player

--- humanPlayer.js
+++ humanPlayer.js
@@ -1,24 +1,32 @@
+const readline = require("readline");
+
 class HumanPlayer {
   constructor() {
     // TODO: Create a new readline interface and store is as an instance
     // variable (this.rl). Remember to require the built-in node module,
     // 'readline'
-    const readline = require("readline");
+    this.rl = readline.createInterface(process.stdin, process.stdout);
 
-    const rl = readline.createInterface(process.stdin, process.stdout);
 
-    rl.question("Whatever prompt you want to ask the user", (answer) => {
-      
-    });
+
+
   }
 
-  getMove() {
+  getMove(processMove) {
+    const rl = readline.createInterface(process.stdin, process.stdout);
+
+    rl.question("Give me two(2) numbers separated by a space (row, column).", (answer) => {
+      processMove(answer.split(' '))
+    });
     // TODO: Ask the user for their move and process the answer using the rl
     // interface.Invoke the callback function (processMove), passing in
     // the given answer in the form of an array representing [row, col]
   }
 
   processGameOver(isWon) {
+    if (isWon) {
+      rl.close();
+    }
     // TODO: Display a different message depending on if the player won or lost
     // the game. Close the rl interface.
   }
